1afc3649d74efd93162c8f1417557e794c64655a
San3le <makalinyane.s@gmail.com>
2023-06-21 14:31:13 +0200

    challenges done

---
+++ IWA_02.js
@@ -0,0 +1,42 @@
+/*Challenge 1*/
+
+/* This is the firstname of the user */
+
+const user = 'John'
+
+
+/* This is the lastname of the user /* const surname = 'Smith'
+
+const user = 'John' // solution
+const surname = 'Smith' // solution
+
+console.log(user; surname;)
+
+
+/*Challenge 2*/
+
+
+/** (c) ACME Inc. 2010 */
+
+/*
+ * This is the date that a user created their account 
+ */
+const date = '10/07/2014'
+
+console(date)
+
+/* SOLUTIONS BELOW*/
+
+/* (c) ACME Inc. 2010 */
+
+/* This is the date that the user created their account*/
+
+const date = '10/07/2014'
+console(date)
+
+/*solution number 2*/
+
+// (c) ACME Inc. 2010
+// This is the date that the user created their account
+const date = '10/07/2014'
+console (date)
